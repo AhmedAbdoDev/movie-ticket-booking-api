92ad2cdd2c740a0636bf0bdd67f7cf766474933b
Ahmed Abd El-Latif <46932157+AhmedAbdoDev@users.noreply.github.com>
2026-08-12 19:04:36 +0300

    chore: set up project foundation

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
 import "dotenv/config";
-import express, { Application } from "express";
+import express from "express";
 import connectDB from "./config/db";
 import { setupSwagger } from "./config/swagger";
+import errorHandler from "./utils/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 connectDB();
+
 import authRouter from "./modules/auth/auth.route";
 
 app.use("/api/auth", authRouter);
 
 setupSwagger(app);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
