ae5534b86b6193a54e73339b16fb76d488cf8acc
AbdullahSamir <abdullahsamirr2006@gmail.com>
2026-08-18 20:13:24 +0300

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
-import { testAuth } from "./auth.service";
+import { login, register } from "./auth.service";
+import { type AuthenticatedRequest } from "../../middlewares/auth.middleware";
 
-export const testAuthController = async (req: Request, res: Response) => {
-  const result = await testAuth(req.body.name);
+export const registerController = async (req: Request, res: Response) => {
+  const result = await register(req.body);
 
-  res.status(200).json(result);
+  res.status(201).json({ success: true, message: "Account created successfully", data: result });
+};
+
+export const loginController = async (req: Request, res: Response) => {
+  const result = await login(req.body);
+
+  res.status(200).json({ success: true, message: "Login successful", data: result });
+};
+
+export const meController = (req: AuthenticatedRequest, res: Response) => {
+  res.status(200).json({ success: true, data: { user: req.user } });
 };
